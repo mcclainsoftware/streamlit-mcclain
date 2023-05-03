a15fa2256be0d65bbd2daf150986d38d03668319
mcclainsoftware <mcclainsoftware@users.noreply.github.com>
2023-05-03 15:00:56 -0400

    adding reference code

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,12 +4,14 @@ import math
 import pandas as pd
 import streamlit as st
 import os
+from llama_index import GPTVectorStoreIndex
+index = GPTVectorStoreIndex.load_from_disk('.\\vector\\vector.json')
 
 if 'response' not in st.session_state:
     st.session_state.response = ''
 
 def send_click():
-    st.session_state.response  = 'TODO: Wire up Index with '+ st.session_state.prompt + os.environ["OPEN_API_KEY"]
+    st.session_state.response  = index.query(st.session_state.prompt +" Only give answers you are sure about given the content of WebTMA knowledge base", verbose=True)
 
 """
 # TMA Question Asker

20c84e28e83605f50ab33ec647f98e0017ea0258
mcclainsoftware <mcclainsoftware@users.noreply.github.com>
2023-05-04 15:26:43 -0400

    attempt at error and vector load

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,24 @@
 
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
+
 import streamlit as st
 import os
 from llama_index import GPTVectorStoreIndex
-
-#index = GPTVectorStoreIndex.load_from_disk('.\\vector\\vector.json')
+from llama_index import download_loader
+from llama_index.node_parser import SimpleNodeParser
+from llama_index import GPTSimpleVectorIndex
+from llama_index import LLMPredictor, GPTSimpleVectorIndex, PromptHelper, ServiceContext
+from langchain import OpenAI
+index = None
+try:
+    index = GPTVectorStoreIndex.load_from_disk('./vector/vector.json')
+except Exception as exc:
+    st.error("Error: "+exc)
 
 if 'response' not in st.session_state:
     st.session_state.response = ''
 
 def send_click():
-    st.session_state.response  = 'dont know' #index.query(st.session_state.prompt +" Only give answers you are sure about given the content of WebTMA knowledge base", verbose=True)
+    st.session_state.response  = 'Not working' if index==None else index.query(st.session_state.prompt +" Only give answers you are sure about given the content of WebTMA knowledge base", verbose=True)
 
 """
 # TMA Question Asker
